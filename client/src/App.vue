b1238f09f64972920bd1ef0f5d4bdf062d11b9e6
EweLoHD <ewelo230@outlook.com>
2022-05-30 11:06:20 +0200

    Added Folder for Frontend

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import SearchBar2 from './components/SearchBar2.vue';
+import SearchBar from './components/SearchBar.vue';
+import 'flowbite';
+
+//var test = ["Italian", "Chinese", "Burger", "Vegetarian"]
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <!--<div class="p-6 w-full max-w-2xl">
+        <SearchBar></SearchBar>
+    </div>-->
+    <a href="#" class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+  </div>
+  
+</template>
---
+++ client/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+
+import 'flowbite';
+
+createApp(App).mount('#app')
