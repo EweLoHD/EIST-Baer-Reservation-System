66f1da39d3d65847a8467996444b6277cbe690e2
EweLoHD <ewelo230@outlook.com>
2022-05-31 10:27:58 +0200

    Added Routing with vue-router

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import './index.css'
 
 import 'flowbite';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SearchBar from '@/components/SearchBar.vue';
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen dark:bg-gray-900">
+    <p class="mb-16 text-5xl font-bold text-gray-900 dark:text-white">Reservation System</p>
+    <div class="mb-32 p-4 w-fit bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <SearchBar></SearchBar>
+    </div>  
+  </div>
+</template>
