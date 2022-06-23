b2f972e593e65a3a68c5af5ae6bea5cf840edcc0
EweLoHD <ewelo230@outlook.com>
2022-06-23 16:27:14 +0200

    Fixed Review

--- server/src/main/java/eist/eistbaer/reservationsystem/restaurant/review/Review.java
+++ server/src/main/java/eist/eistbaer/reservationsystem/restaurant/review/Review.java
@@ -1,5 +1,6 @@
 package eist.eistbaer.reservationsystem.restaurant.review;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import eist.eistbaer.reservationsystem.restaurant.reviewrating.ReviewRating;
 
 import javax.persistence.Entity;
@@ -10,9 +11,51 @@ import javax.persistence.Id;
 public class Review {
     @Id
     @GeneratedValue
+    @JsonIgnore
     private Long id;
 
     private String comment;
 
     private ReviewRating rating;
+
+    public Review() {
+    }
+
+    public Review(String comment, ReviewRating rating) {
+        this.comment = comment;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public ReviewRating getRating() {
+        return rating;
+    }
+
+    public void setRating(ReviewRating rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
 }
