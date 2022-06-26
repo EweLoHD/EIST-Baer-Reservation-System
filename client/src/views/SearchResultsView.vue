5e47decb2b2bbdd189591c4d944e2f6ea3783380
EweLoHD <ewelo230@outlook.com>
2022-06-26 23:47:09 +0200

    Fixed #5

--- client/src/views/SearchResultsView.vue
+++ client/src/views/SearchResultsView.vue
@@ -161,7 +161,7 @@ export default {
         </div>
 
         <!--Map Modal-->
-        <div id="map-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-4 right-0 left-0 z-50 w-full md:inset-0 md:h-modal h-modal">
+        <div id="map-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-4 right-0 left-0 z-50 w-full h-modal">
             <div class="relative p-4 w-full max-w-7xl h-auto">
                 <div class="relative bg-white rounded-lg shadow-2xl dark:bg-gray-700">
                     <!--Close Button-->
@@ -170,7 +170,7 @@ export default {
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
                         </button>
                     </div>
-                    <div class="space-y-6 h-192 w-full">
+                    <div class="space-y-6 h-[80vh] w-full">
                         <div id="map" class="h-full relative rounded-lg">
 
                         </div>
